d908caf7a3e28eb3d5c9863efbdc0b903e205d09
zzzrrrddd <rzha0233@student.monash.edu>
2026-06-30 22:42:17 +0800

    Initial commit: week1

---
+++ week1/src/components/MyName.vue
@@ -0,0 +1,16 @@
+<script setup>
+const name = 'Rundong Zhang'
+</script>
+
+<template>
+  <p class="my-name">Welcome, {{ name }}!</p>
+</template>
+
+<style scoped>
+.my-name {
+  font-size: 1.5rem;
+  color: #42b883;
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
