40e77924f4bb2f32d5452336f75534d5c0ed751c
zzzrrrddd <rzha0233@student.monash.edu>
2026-07-29 11:00:07 +0800

    tt

--- NoMash-Library/src/router/index.js
+++ NoMash-Library/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import LoginView from '../views/LoginView.vue'
@@ -72,7 +72,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
